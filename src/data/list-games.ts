ba5befeb262e4d3bc2eb51a56b5aa8629eb59198
kangyann <kangyann727@gmail.com>
2026-04-05 15:36:10 +0700

    feat: restore list-games for temporaly;

---
+++ src/data/list-games.ts
@@ -0,0 +1,22 @@
+/**
+ * @constant
+ * @exports ListGames
+ * @type {string[]}
+ */
+
+export type ListGamesTypes = Record<"name" | "prefix", string>;
+
+export const ListGames: ListGamesTypes[] = [
+   {
+      name: "Mobile Legends",
+      prefix: "mobile-legends",
+   },
+   {
+      name: "Free Fire",
+      prefix: "free-fire",
+   },
+   {
+      name: "Point Blank",
+      prefix: "point-blank",
+   },
+];
